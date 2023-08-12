ef6883fade4a0d1b79d83b9b80bb0a2e480f78ce
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-08-12 15:23:19 +0300

    vika

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+import { FiSearch } from 'react-icons/fi';
+import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+
+export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+  render() {
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          value={this.state.query}
+          onChange={this.handleChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
+  }
+}
---
+++ src/tabs/Gallery.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+
+import * as ImageService from 'service/image-service';
+import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+
+export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    photos: [],
+  };
+  async componentDidUpdate(prevProp, prevState) {
+    if (
+      prevState.query === this.state.query ||
+      this.state.query.trim() === ''
+    ) {
+      return;
+    }
+    const { photos, total_results } = await ImageService.getImages({
+      page: this.state.page,
+      query: this.state.query,
+    });
+    this.setState(prev => ({ photos: [...prev.photos, ...photos] }));
+  }
+  onSubmit = query => {
+    this.setState({ query });
+  };
+  render() {
+    return (
+      <>
+        <SearchForm onSubmit={this.onSubmit} />
+        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+      </>
+    );
+  }
+}
