2dc4975ac230834d0b5989e784be5028b51479bf
Egor Sergeevich <122815068+EgorSereevich@users.noreply.github.com>
2023-08-12 16:10:53 +0300

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,29 +8,65 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     photos: [],
+    lastPage: true,
+    isLoading: false,
   };
+  getNormalayzedImages = array =>
+    array.map(({ id, avg_color, alt, src }) => ({ id, avg_color, alt, src }));
   async componentDidUpdate(prevProp, prevState) {
     if (
-      prevState.query === this.state.query ||
+      (prevState.query === this.state.query &&
+        prevState.page === this.state.page) ||
       this.state.query.trim() === ''
     ) {
       return;
     }
     // bbbb
-    const { photos, total_results } = await ImageService.getImages({
-      page: this.state.page,
-      query: this.state.query,
-    });
-    this.setState(prev => ({ photos: [...prev.photos, ...photos] }));
+    this.setState({ isLoading: true });
+    try {
+      const { photos, total_results } = await ImageService.getImages({
+        page: this.state.page,
+        query: this.state.query,
+      });
+      this.setState(prev => ({
+        photos: [...prev.photos, ...this.getNormalayzedImages(photos)],
+        lastPage: this.state.page >= Math.ceil(total_results / 15),
+      }));
+    } catch (error) {
+      console.log('Error!!');
+    } finally {
+      this.setState({ isLoading: false });
+    }
   }
   onSubmit = query => {
-    this.setState({ query });
+    this.setState({ query, page: 1, photos: [] });
+  };
+  handleClick = evt => {
+    this.setState(prev => ({ page: prev.page + 1 }));
   };
   render() {
+    const { photos, lastPage, isLoading } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {isLoading && <p>Loading</p>}
+        {photos.length === 0 && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {photos.length !== 0 && (
+          <Grid>
+            {photos.map(({ id, avg_color, alt, src }) => {
+              return (
+                <GridItem key={id}>
+                  <CardItem color={avg_color}>
+                    <img src={src.medium} alt={alt} />
+                  </CardItem>
+                </GridItem>
+              );
+            })}
+          </Grid>
+        )}
+        {!lastPage && <Button onClick={this.handleClick}>Load more</Button>}
       </>
     );
   }
